bd27d852b3cb27f697eeab19f4557b35a9d5cdb8
wolfreezark <oxneedless@gmail.com>
2017-05-25 23:26:21 -0400

    Update Theos4Windows.bat

--- Theos4Windows.bat
+++ Theos4Windows.bat
@@ -3,10 +3,10 @@ if "%PROCESSOR_ARCHITECTURE%"=="AMD64" goto 64BIT
 mkdir C:\tmp
 mkdir C:\tmp\theos
 cd C:\tmp\
-powershell (new-object System.Net.WebClient).DownloadFile('https://www.cygwin.com/setup-x86.exe', 'C:\tmp\setup-x86.exe')
+powershell (new-object System.Net.WebClient).DownloadFile('https://www.cygwin.com/setup-x86.exe', 'c:\tmp\setup-x86.exe')
 setup-x86.exe -q --root C:\cygwin --site http://cygwin.mirror.constant.com --no-admin -d -P wget,git,ca-certificates,make perl,openssh,curl,curl-debuginfo,libcurl-devel,libcurl-doc,libcurl4,php-curl
-c:\cygwin\bin\bash -l -c "git clone https://github.com/wolfreezark/theos2.git /cygdrive/c/tmp/theos/"
-c:\cygwin\bin\bash -l -c "chmod u+x /cygdrive/c/tmp/theos/install.sh"
+C:\cygwin\bin\bash -l -c "git clone https://github.com/wolfreezark/theos2.git /cygdrive/c/tmp/theos/"
+C:\cygwin\bin\bash -l -c "chmod u+x /cygdrive/c/tmp/theos/install.sh"
 goto END
 :64BIT
 mkdir C:\tmp
@@ -14,11 +14,11 @@ mkdir C:\tmp\theos
 cd C:\tmp
 powershell (new-object System.Net.WebClient).DownloadFile('https://www.cygwin.com/setup-x86_64.exe', 'C:\tmp\setup-x86.exe')
 setup-x86.exe -q --root C:\cygwin --site http://cygwin.mirror.constant.com --no-admin -d -P wget,git,ca-certificates,make perl,openssh,curl,curl-debuginfo,libcurl-devel,libcurl-doc,libcurl4,php-curl
-c:\cygwin\bin\bash -l -c "git clone https://github.com/wolfreezark/theos2.git /cygdrive/c/tmp/theos/"
-c:\cygwin\bin\bash -l -c "chmod u+x /cygdrive/c/tmp/theos/install.sh"
+C:\cygwin\bin\bash -l -c "git clone https://github.com/wolfreezark/theos2.git /cygdrive/c/tmp/theos/"
+C:\cygwin\bin\bash -l -c "chmod u+x /cygdrive/c/tmp/theos/install.sh"
 :END
-c:\cygwin\bin\bash -l -c /cygdrive/c/tmp/theos/install.sh
+C:\cygwin\bin\bash -l -c /cygdrive/c/tmp/theos/install.sh
 cls
-echo Type ~/theos/bin/nic.pl in Cygwin terminal to run Theos, press any key to exit this installer.
+echo Type $THEOS/bin/nic.pl in Cygwin terminal to run Theos, press any key to exit this installer.
 timeout 300
 rmdir C:\tmp /s /q
